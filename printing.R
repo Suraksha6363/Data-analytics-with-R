e9be3625ff45be688bfef471ac26901aec4e1dfa
suraksha shetty <surakshashetty2468@gmail.com>
2024-09-16 11:56:04 +0530

    Add files via upload

---
+++ printing.R
@@ -0,0 +1,22 @@
+#to print the string on the display
+print("hello")
+print("hello",4)
+print("hello","world")
+print(7)
+ 
+#to print the string on the display which is separated by another string in the middle
+paste("hello","world",sep='6')
+
+#to take input from the user and print it on the screen 
+n=readline("enter your name:")
+paste("hello all my name is ",n)
+
+n=readline("enter your name:")
+print("hello all my name is ",n)
+
+#if we use print function we will get error
+n=readline("enter your name:")
+sprint("hello all my name is %s ",n)
+
+
+ 
