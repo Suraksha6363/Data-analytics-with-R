bba595280300e18a2531a22604000a0db96d1440
suraksha shetty <surakshashetty2468@gmail.com>
2024-09-16 11:46:12 +0530

    Add files via upload

---
+++ class and type .R
@@ -0,0 +1,19 @@
+#to find class of a variable
+c="hello"
+class(c)
+
+#to find type of a variable
+c="hello"
+typeof(c)
+
+#the class and type of a variable doesn't change with upper or lower case
+d= "Hello"
+class(d)
+typeof(d)
+
+e='H'
+f="R"
+class(e)
+class(f)
+typeof(e)
+typeof(f)
