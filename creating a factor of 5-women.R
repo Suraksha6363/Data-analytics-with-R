cb09b768ca5e0c4ec61b39fb732ef35c17bfb8f2
suraksha shetty <surakshashetty2468@gmail.com>
2024-09-29 19:46:54 +0530

    Add files via upload

---
+++ creating a factor of 5-women.R
@@ -0,0 +1,4 @@
+#write a R program to create a factor corresponding to the height of 5-women .define the factor as ordered
+height=c("5ft","4ft","3ft","5.7ft","5.4ft")
+f=factor(height,ordered=TRUE)
+f
